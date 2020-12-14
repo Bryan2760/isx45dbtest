b12e5bc493682d8a174ff5b05d03e280903b8607
Bryan <bryanwong@student.csulb.edu>
2020-12-14 15:02:40 -0800

    project

--- dblib.js
+++ dblib.js
@@ -9,7 +9,7 @@ const pool = new Pool({
 });
 
 const getTotalRecords = () => {
-    sql = "SELECT COUNT(*) FROM car";
+    sql = "SELECT COUNT(*) FROM customer";
     return pool.query(sql)
         .then(result => {
             return {
@@ -19,76 +19,166 @@ const getTotalRecords = () => {
         })
         .catch(err => {
             return {
-                msg: `Error ${err.message}`
+                msg: `Error: ${err.message}`
             }
         });
 };
 
-const insertCar = (car) => {
-
-    if (car instanceof Array) {
-        params = car;
+const insertCustomer = (customer) => {
+    
+    if (customer instanceof Array) {
+        params = customer;
     } else {
-        params = Object.values(car);
+        params = Object.values(customer);
     };
 
-    const sql = `INSERT INTO car (carvin, carmake, carmodel, carmileage)
-                 VALUES ($1, $2, $3, $4)`;
+    const sql = `INSERT INTO customer (cusid, cusfname, cuslname, cusstate, cussalesytd, cussalesprev)
+                 VALUES ($1, $2, $3, $4, $5, $6)`;
 
     return pool.query(sql, params)
         .then(res => {
             return {
                 trans: "success", 
-                msg: `car id ${params[0]} successfully inserted`
+                msg: `customer id ${params[0]} successfully inserted`
             };
         })
         .catch(err => {
             return {
                 trans: "fail", 
-                msg: `Error on insert of car id ${params[0]}.  ${err.message}`
+                msg: `Error on insert of customer id ${params[0]}.  ${err.message}`
             };
         });
 };
 
-const findCar = (car) => {
-    // Will build query based on data provided from the form
-    //  Use parameters to avoid sql injection
+const reportCustomer = () => {
 
-    // Declare variables
+ 
+    const sql = "SELECT * FROM customer ORDER BY cuslname asc";
+
+    return pool.query(sql)
+    .then(report => {
+        return {
+            trans: "success",  
+            repCus: report.rows  
+    }
+    })
+    .catch(err =>{
+        return{
+            trans: "fail",
+            msg: `${err.message}`
+        }
+    })
+
+};
+
+const reportSales = () => {
+
+ 
+    const sql = "SELECT * FROM customer ORDER BY cussalesytd desc";
+
+    return pool.query(sql)
+    .then(report => {
+        return {
+            trans: "success",  
+            repSal: report.rows  
+    }
+    })
+    .catch(err =>{
+        return{
+            trans: "fail",
+            msg: `${err.message}`
+        }
+    })
+
+};
+
+const reportRandom = () => {
+
+    const sql = "SELECT * FROM customer ORDER BY RANDOM() LIMIT 3";
+
+    return pool.query(sql)
+    .then(report => {
+        return {
+            trans: "success",
+            repRan: report.rows
+        }
+
+    })
+    .catch(err =>{
+        return {
+            trans:"fail",
+            msg: `${err.message}`
+        }
+    })
+};
+
+const importCustomer = (params) => {
+
+    const sql = "INSERT INTO customer(cusid, cusfname, cuslname, cusstate, cussalesytd, cussalesprev) VALUES ($1, $2, $3, $4, $5, $6)";
+   
+    return pool.query(sql, params)
+
+        .then(res => {
+            return {
+                trans: "success", 
+                result: res.rows
+            };
+        })
+        .catch(err => {
+            return {
+                trans: "fail", 
+                msg: `${err.message}`
+            };
+        });
+}
+
+const findCustomer = (customer) => {
+    
+
     var i = 1;
     params = [];
-    sql = "SELECT * FROM car WHERE true";
+    sql = "SELECT * FROM customer WHERE true";
 
-    // Check data provided and build query as necessary
-    if (car.carvin !== "") {
-        params.push(parseInt(car.carvin));
-        sql += ` AND carvin = $${i}`;
+
+    if (customer.cusid !== "") {
+        params.push(parseInt(customer.cusid));
+        sql += ` AND cusid = $${i}`;
         i++;
     };
-    if (car.carmake !== "") {
-        params.push(`${car.carmake}%`);
-        sql += ` AND UPPER(carmake) LIKE UPPER($${i})`;
+    if (customer.cusfname !== "") {
+        params.push(`${customer.cusfname}%`);
+        sql += ` AND UPPER(cusfname) LIKE UPPER($${i})`;
         i++;
     };
-    if (car.carmodel !== "") {
-        params.push(`${car.carmodel}%`);
-        sql += ` AND UPPER(carmodel) LIKE UPPER($${i})`;
+    if (customer.cuslname !== "") {
+        params.push(`${customer.cuslname}%`);
+        sql += ` AND UPPER(cuslastname) LIKE UPPER($${i})`;
         i++;
     };
-    if (car.carmileage !== "") {
-        params.push(parseFloat(car.carmileage));
-        sql += ` AND carmileage >= $${i}`;
+    if (customer.cusstate !== "") {
+        params.push(`${customer.cusstate}%`);
+        sql += ` AND UPPER(cusstate) LIKE UPPER($${i})`;
+        i++;
+    };
+    if (customer.cussalesytd !== "") {
+        params.push(parseFloat(customer.cussalesytd));
+        sql += ` AND cussalesytd >= $${i}`;
+        i++;
+    };
+    if (customer.cussalesprev !== "") {
+        params.push(parseFloat(customer.cussalesprev));
+        sql += ` AND cussalesprev >= $${i}`;
         i++;
     };
 
-    sql += ` ORDER BY carvin`;
-    // for debugging
-    console.log("sql: " + sql);
-    console.log("params: " + params);
+    sql += ` ORDER BY cusid`;
+
+     console.log("sql: " + sql);
+     console.log("params: " + params);
 
     return pool.query(sql, params)
         .then(result => {
-            return {
+            return { 
                 trans: "success",
                 result: result.rows
             }
@@ -101,6 +191,10 @@ const findCar = (car) => {
         });
 };
 
-module.exports.findCar = findCar;
-module.exports.insertCar = insertCar;
+module.exports.importCustomer = importCustomer;
+module.exports.reportRandom = reportRandom;
+module.exports.reportSales = reportSales;
+module.exports.reportCustomer = reportCustomer;
+module.exports.findCustomer = findCustomer;
+module.exports.insertCustomer = insertCustomer;
 module.exports.getTotalRecords = getTotalRecords;
--- index.js
+++ index.js
@@ -1,80 +1,443 @@
-// Required modules 
 const express = require("express");
 const app = express();
 const dblib = require("./dblib.js");
 const path = require("path");
 const multer = require("multer");
 const upload = multer();
+const { Pool } = require('pg');
+const { response } = require("express");
 
-app.use(express.urlencoded({ extended: false }));
-// npm start
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+require('dotenv').config()
 
 
-// Setup EJS
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
+//Home
+app.get("/", (req, res) => {
+  {
+    res.render("index");
+  }
 });
 
 
 
-// Application folders
-app.use(express.static("public"));
+//Manage
+app.get("/manage", async (req, res) => {
 
-// Start listener
-app.listen(process.env.PORT || 3000, () => {
-    console.log("Server started (http://localhost:3000/) !");
+  const totRecs = await dblib.getTotalRecords();
+
+  const customers = {
+    cusid: "",
+    cusfname: "",
+    cuslname: "",
+    cusstate: "",
+    cussalesytd: "",
+    cussalesprev: ""
+  };
+
+  res.render("manage", {
+    type: "get",
+    totRecs: totRecs.totRecords,
+    customer: customers
+  });
+
+
+
 });
-//Setup routes
+
+app.post("/manage", async (req, res) => {
+
+  const totRecs = await dblib.getTotalRecords();
 
 
-app.get("/", async (req, res) => {
-
-    const totRecs = await dblib.getTotalRecords();
-
-    const car = {
-        carvin: "",
-        carmake: "",
-        carmodel: "",
-        carmileage: ""
-    };
-    res.render("index", {
-        type: "get",
+  dblib.findCustomer(req.body)
+    .then(result => {
+      res.render("manage", {
+        type: "post",
         totRecs: totRecs.totRecords,
-        car: car
+        result: result,
+        customer: req.body
+      });
+    })
+    .catch(err => {
+      res.render("manage", {
+        type: "post",
+        totRecs: totRecs.totRecords,
+        result: `Unexpected Error: ${err.message}`,
+        customer: req.body
+      });
     });
+
+});
+
+
+// get create
+app.get("/create", (req, res) => {
+
+  res.render("create", {
+    model: {},
+    type: "get"
+  });
 });
 
 
 
-app.post("/", async (req, res) => {
+// post create
+app.post("/create", async (req, res) => {
+  // console.log(req.body);
+  const sql = "INSERT INTO customer (cusid, cusfname, cuslname, cusstate, cussalesytd, cussalesprev) VALUES ($1, $2, $3, $4, $5, $6)";
+  const customer = [req.body.cusid, req.body.cusfname, req.body.cuslname, req.body.cusstate, req.body.cussalesytd, req.body.cussalesprev];
+  const show = req.body;
+
+  try {
+
+    const resultNew = await pool.query(sql, customer);
+
+    res.render("create", {
+      model: show,
+      resultNew: resultNew.rowCount,
+      type: "POST"
+    });
+
+  } catch (err) {
+
+    res.render("create", {
+      model: show,
+      resultNew: err.message,
+      type: "POST"
+    });
+  }
+});
+
+
+
+// GET /edit
+app.get("/edit/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = "SELECT * FROM customer WHERE cusid = $1";
+  pool.query(sql, [id], (err, result) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.render("edit", {
+      model: result.rows[0],
+      type: "get"
+    });
+  });
+});
+
+// POST /edit
+app.post("/edit/:id", async (req, res) => {
+  const id = req.params.id;
+  const model = [req.body.cusfname, req.body.cuslname, req.body.cusstate, req.body.cussalesytd, req.body.cussalesprev, id];
+  const show = req.body;
+  console.log("The show is", show);
+
+  const sql = "UPDATE customer SET cusfname = $1, cuslname = $2, cusstate = $3, cussalesytd = $4, cussalesprev = $5 WHERE (cusid = $6)";
+
+  try {
+
+    const resultEdit = await pool.query(sql, model);
+
+    res.render("edit", {
+      model: show,
+      resultEdit: resultEdit.rowCount,
+      type: "POST"
+    });
+
+  } catch (err) {
+
+    res.render("edit", {
+      model: show,
+      resultEdit: err.message,
+      type: "POST"
+    })
+  };
+});
+
+
+//GET /delete
+app.get("/delete/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = "SELECT * FROM customer WHERE cusid =$1";
+
+  pool.query(sql, [id], (err, result) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log(result.rows[0]);
+    res.render("delete", {
+      model: result.rows[0],
+      type: "get"
+    });
+  });
+});
+
+//POST /delete
+app.post("/delete/:id", async (req, res) => {
+  const id = req.params.id;
+  const model = req.body;
+  console.log("the model is", model);
+  const sql = "DELETE FROM customer WHERE cusid = $1";
+
+  try {
+    const resultDel = await pool.query(sql, [id]);
+
+    res.render("delete", {
+      model: model,
+      resultDel: resultDel.rowCount,
+      type: "POST"
+    });
+
+  } catch (err) {
+    res.render("delete", {
+      model: model,
+      resultDel: err.message,
+      type: "POST"
+    });
+    console.log(err.message);
+  }
+
+
+
+});
+
+
+//Get /reports
+app.get("/reports", async (req, res) => {
+
+  const reports = req.body.value;
+
+  res.render("reports", {
+    type: "get",
+    model: reports,
+    cust: "",
+    sale: "",
+    ran: ""
+  });
+});
+
+//POST /reports
+app.post("/reports", async (req, res) => {
+  console.log(req.body);
+  var report;
+  var trans;
+  var x;
+  var y;
+  var z;
+
+  const model = req.body;
+
+  try {
+
+    if (req.body.reports === "1") {
+
+      const cusReport = await dblib.reportCustomer();
+      report = cusReport.repCus;
+      trans = cusReport.trans;
+      y = "selected";
+
+    } else if (req.body.reports === "2") {
+
+      const cusSales = await dblib.reportSales();
+
+      if (cusSales.trans === "success") {
+        report = cusSales.repSal;
+        trans = cusSales.trans;
+        x = "selected";
+
+      } else {
+
+        report = cusSales.msg;
+        trans = cusSales.trans;
+        console.log("Error!", report);
+        z = "selected"
+
+      }
+    } else {
+
+      const cusRandom = await dblib.reportRandom();
+      report = cusRandom.repRan;
+      trans = cusRandom.trans;
+      z = "selected"
+    }
+
+    console.log(x, y, z);
+    res.render("reports", {
+      type: "POST",
+      report: report,
+      trans: trans,
+      model: model,
+      value: req.body.reports,
+      cust: y,
+      sale: x,
+      ran: z
+    });
+  } catch (err) {
+
+    res.render("reports", {
+      type: "POST",
+      trans: "fail",
+      report: err.message,
+      model: model,
+      value: req.body.reports
+
+    });
+
+    console.log(err.message);
+
+  }
+});
+
+// GET /import
+app.get("/import", async (req, res) => {
+  const totRecs = await dblib.getTotalRecords();
+  const customers = {
+
+    cusid: "",
+    cusfname: "",
+    cuslname: "",
+    cusstate: "",
+    cussalesytd: "",
+    cussalesprev: ""
+
+  };
+
+  res.render("import", {
+    type: "get",
+    totRecs: totRecs.totRecords,
+    customer: customers
+  });
+});
+
+// POST /import
+app.post("/import", upload.single('filename'), async (req, res) => {
+
+  (async () => {
+    var numInserted = 0;
+    var numFailed = 0;
+    var errorMessage = "";
+    const buffer = req.file.buffer;
+    const lines = buffer.toString().split(/\r?\n/);
+
+
+
+    for (line of lines) {
+      
+      const customer = line.split(",");
+      console.log(customer);
+      
+      console.log("Wait for Result")
+
+      const result = await dblib.importCustomer(customer);
+
+      if (result.trans === "success") {
+
+        numInserted++;
+
+      } else {
+
+        numFailed++;
+
+        errorMessage += `${result.msg} \r\n`;
+      };
+    };
+
+    console.log("Import Summary");
+    console.log(`Records processed: ${numInserted + numFailed}`);
+    console.log(`Records successfully inserted: ${numInserted}`);
+    console.log(`Records with insertion errors: ${numFailed}`);
+
+    if (numFailed > 0) {
+
+      console.log("Error Details:");
+
+      console.log(errorMessage);
+    };
 
     const totRecs = await dblib.getTotalRecords();
 
-    dblib.findCar(req.body)
-        .then(result => {
-            console.log("test",result);
-            res.render("index", {
-                type: "post",
-                totRecs: totRecs.totRecords,
-                result: result,
-                car: req.body
-            })
-        })
-        .catch(err => {
-            console.log("testerror",result);
-            res.render("index", {
-                type: "post",
-                totRecs: totRecs.totRecords,
-                result: `Unexpected Error: ${err.message}`,
-                car: req.body
-            });
-        });
+    res.render("import", {
+      type: "POST",
+      totRecs: totRecs.totRecords,
+      numFailed: numFailed,
+      numInserted: numInserted,
+      errorMessage: errorMessage
+    })
+
+  })()
+
+});
+
+
+
+
+
+
+// GET /export
+app.get("/export", async (req, res) => {
+  const totRecs = await dblib.getTotalRecords();
+  var message = "";
+
+  const customers = {
+    cusid: "",
+    cusfname: "",
+    cuslname: "",
+    cusstate: "",
+    cussalesytd: "",
+    cussalesprev: ""
+  };
+
+  res.render("export", {
+    type: "get",
+    message: message,
+    totRecs: totRecs.totRecords,
+    customer: customers
+  });
+
+});
+
+// POST /export
+app.post("/export", (req, res) => {
+  const sql = "SELECT cusid, cusfname, cuslname, cusstate, cussalesytd::numeric, cussalesprev::numeric FROM customer";
+  pool.query(sql, [], (err, result) => {
+    var message = "";
+    if (err) {
+      message = `Error - ${err.message}`;
+      res.render("export", { message: message })
+    } else {
+      var output = "";
+      result.rows.forEach(customer => {
+        output += `${customer.cusid},${customer.cusfname},${customer.cuslname},${customer.cusstate},${customer.cussalesytd},${customer.cussalesprev}\r\n`;
+      });
+      res.header("Content-Type", "text/csv");
+      res.attachment("export.txt");
+      return res.send(output);
+    };
+
+  });
 });
